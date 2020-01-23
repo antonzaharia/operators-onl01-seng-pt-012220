29c6a0130fcac2c0fa268dc321ab98b8ba536943
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-01-23 15:08:21 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,10 +1,19 @@
 def unsafe?(speed)
-
+  if speed > 60
+    return TRUE
+  elsif speed < 40
+    return TRUE
+  else
+    return FALSE
+  end
 end
 
 
 
 def not_safe?(speed)
+	speed > 60 ? TRUE : FALSE
+	end
+	speed < 40 ? TRUE : FALSE
 	
 end
 	
